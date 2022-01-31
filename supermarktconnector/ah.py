f91c30041c7a6d03f3a2fdec05277b4f5ccacc36
Bart Machielsen <bartmachielsen@gmail.com>
2022-01-31 20:53:42 +0100

    0.8 - Albert Heijn Bonus products iteration impl

--- supermarktconnector/ah.py
+++ supermarktconnector/ah.py
@@ -1,5 +1,6 @@
 import requests
 from pprint import pprint
+from datetime import datetime
 
 HEADERS = {
     'Host': 'api.ah.nl',
@@ -91,15 +92,77 @@ class AHConnector:
             response.raise_for_status()
         return response.json()
 
+    def get_bonus_periods(self):
+        """
+        Information about the current bonus periods active.
+        Returns a list with all periods, each period has a start and end date and sections, to view the products
+        you need the sections within 'urlMetadataList' using 'get_bonus_periods_products'
+        """
+        response = requests.get(
+            'https://api.ah.nl/mobile-services/bonuspage/v1/metadata',
+            headers={**HEADERS, "Authorization": "Bearer {}".format(self._access_token.get('access_token'))}
+        )
+        if not response.ok:
+            response.raise_for_status()
+        return response.json()['periods']
+
+    def get_bonus_periods_groups_or_products(self, url):
+        response = requests.get(
+            f'https://api.ah.nl/mobile-services/{url}',
+            headers={**HEADERS, "Authorization": "Bearer {}".format(self._access_token.get('access_token'))}
+        )
+        if not response.ok:
+            response.raise_for_status()
+        return response.json()
+
+    def get_bonus_group_products(self, group_id, date):
+        response = requests.get(
+            f'https://api.ah.nl/mobile-services/bonuspage/v1/segment',
+            headers={**HEADERS, "Authorization": "Bearer {}".format(self._access_token.get('access_token'))},
+            params={"date": date.strftime('%Y-%m-%d'), "segmentId": group_id, "includeActivatableDiscount": "false"}
+        )
+        if not response.ok:
+            response.raise_for_status()
+        return response.json()
+
+    def get_all_bonus_products(self, date=None):
+        """
+        Get all available bonus products in a certain period, pass a datetime or date object to filter on a specific date
+        default is None which means current date
+        """
+        date = date or datetime.today()
+
+        for period in self.get_bonus_periods():
+            if date < datetime.strptime(period['bonusStartDate'], '%Y-%m-%d') or date > datetime.strptime(period['bonusEndDate'], "%Y-%m-%d"):
+                continue
+
+            for meta_data in period['urlMetadataList']:
+                for bon_group_or_prod in \
+                        self.get_bonus_periods_groups_or_products(meta_data['url'])['bonusGroupOrProducts']:
+
+                    if bon_group_or_prod.get('product'):
+                        yield bon_group_or_prod['product']
+
+                    if bon_group_or_prod.get('bonusGroup'):
+                        for product in \
+                                self.get_bonus_group_products(bon_group_or_prod['bonusGroup']['id'], date).get('products', []):
+                            yield product
+
 
 if __name__ == '__main__':
     connector = AHConnector()
-    # print(connector.search_products())
-    # print(len(list(connector.search_all_products(query='smint'))))
-    # print(connector.get_product_details(connector.get_product_by_barcode('8410031965902')))
-    # print(connector.get_categories())
-    # print(connector.get_sub_categories(connector.get_categories()[0]))
+    # pprint(connector.search_products())
+    # pprint(len(list(connector.search_all_products(query='smint'))))
+    # pprint(connector.get_product_details(connector.get_product_by_barcode('8410031965902')))
+    # pprint(connector.get_categories())
+    # pprint(connector.get_sub_categories(connector.get_categories()[0]))
 
-    # print(connector.search_products('Smint')['products'])
+    # pprint(connector.search_products('Smint')['products'])
 
-    pprint(connector.get_product_details(177119))
+    # pprint(connector.get_product_details(177119))
+
+    # pprint(connector.get_bonus_periods())
+    # pprint(connector.get_bonus_periods_products(connector.get_bonus_periods()[0]['urlMetadataList'][0]['url']))
+
+    for bonus_prod in connector.get_all_bonus_products():
+        print(bonus_prod)
